75711d5fdafd3121a494a99ac22d00df2d369344
Julian Centurion <julian.centurion@mercadolibre.com>
2022-06-25 18:22:25 -0300

    View binding

--- ExpensesReport/ExpensesList/ExpensesListViewController.swift
+++ ExpensesReport/ExpensesList/ExpensesListViewController.swift
@@ -16,14 +16,32 @@ class ExpensesListViewController: UIViewController {
     @IBOutlet weak var progressBar: UIProgressView!
     @IBOutlet weak var balanceBackgroundView: UIView!
 
+    // MARK: - View Model
+    private let viewModel: ExpensesListViewModel
+
+    init(withViewModel viewModel: ExpensesListViewModel = ExpensesListViewModel()) {
+        self.viewModel = viewModel
+        super.init(nibName: nil, bundle: nil)
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
         setUpBalanceView()
-
-        // Do any additional setup after loading the view.
+        bindingViewModel()
+        viewModel.loadExpensesData()
     }
 
     func setUpBalanceView() {
         balanceBackgroundView.rondedView()
+    }
+
+    func bindingViewModel() {
+        viewModel.bindViewModelToController = { expesesData in
+            print(expesesData)
+        }
     }
 }
