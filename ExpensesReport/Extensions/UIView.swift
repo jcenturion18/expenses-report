5dab37c8cf1fbdb0079eba0cbfd0d42a799d3303
Julian Centurion <julian.centurion@mercadolibre.com>
2022-06-25 17:23:56 -0300

    Create UIView extension

---
+++ ExpensesReport/Extensions/UIView.swift
@@ -0,0 +1,20 @@
+//
+//  UIView.swift
+//  ExpensesReport
+//
+//  Created by Julian Centurion on 25/06/2022.
+//
+
+import Foundation
+import UIKit
+
+extension UIView {
+    
+    func rondedView() {
+        backgroundColor = .white
+        layer.cornerRadius = 6
+        clipsToBounds = true
+        layer.borderWidth = 1
+        layer.borderColor = UIColor.black.cgColor
+    }
+}
