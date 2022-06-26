76eb0ff780ca2af40cd4dc7e21058a5cbb5cd77f
Julian Centurion <julian.centurion@mercadolibre.com>
2022-06-26 01:54:39 -0300

    First atempt

--- ExpensesReport/ExpensesList/ExpensesTableViewDataSource.swift
+++ ExpensesReport/ExpensesList/ExpensesTableViewDataSource.swift
@@ -7,6 +7,34 @@
 
 import UIKit
 
-class ExpensesTableViewDataSource: NSObject {
+class ExpensesTableViewDataSource: NSObject, UITableViewDataSource {
 
+    let expenses: [ExpensesByDay]
+    init(withExpensesByDayArray data: [ExpensesByDay]) {
+        self.expenses = data
+        super.init()
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        expenses[section].expenses.count + 1
+    }
+
+    func numberOfSections(in tableView: UITableView) -> Int {
+        expenses.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        var cell = tableView.dequeueReusableCell(withIdentifier: "Cell")
+
+        if  !(cell != nil) {
+            cell = UITableViewCell(style: .default, reuseIdentifier: "Cell")
+        }
+
+        if indexPath.row == 0 {
+            cell!.textLabel?.text = expenses[indexPath.section].title
+        } else {
+            cell!.textLabel?.text = expenses[indexPath.section].expenses[indexPath.row - 1].description
+        }
+        return cell!
+    }
 }
