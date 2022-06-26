dc98d92afb35243fdfaa91980d9205b35fd974a5
Julian Centurion <julian.centurion@mercadolibre.com>
2022-06-26 01:51:54 -0300

    Create ExpensesTableViewDataSource

---
+++ ExpensesReport/ExpensesList/ExpensesTableViewDataSource.swift
@@ -0,0 +1,12 @@
+//
+//  ExpensesTableViewDataSource.swift
+//  ExpensesReport
+//
+//  Created by Julian Centurion on 26/06/2022.
+//
+
+import UIKit
+
+class ExpensesTableViewDataSource: NSObject {
+
+}
