0928d1c7d9f2304e0881a7e2d38a1305d76a1459
Julian Centurion <julian.centurion@mercadolibre.com>
2022-06-26 01:08:49 -0300

    Clean code

--- ExpensesReport/ExpensesList/ExpensesListViewModel.swift
+++ ExpensesReport/ExpensesList/ExpensesListViewModel.swift
@@ -18,23 +18,33 @@ class ExpensesListViewModel {
     var bindViewModelToController : ((_ expenses: ExpensesListData) -> Void) = {_ in }
 
     func loadExpensesData() {
-        let expenses = Expenses(expenses:
-                                [
-                                    Expense(description: "Coffee", date: Date.now, amount: 7, transaction: .expense),
-                                    Expense(description: "Salary", date: Date.now, amount: 10, transaction: .income)
-                                ]
-        )
+        let expenses = [
+            Expense(description: "Salary", date: "30/05/2022".toDate(), amount: 10, transaction: .income),
+            Expense(description: "Coffee", date: "26/05/2022".toDate(), amount: 7, transaction: .expense),
+            Expense(description: "Salary", date: "26/05/2022".toDate(), amount: 10, transaction: .income)
+        ]
+
         expensesListData = proccess(expenses: expenses)
 
     }
 
-    func proccess(expenses data: Expenses) -> ExpensesListData {
+    func proccess(expenses: [Expense]) -> ExpensesListData {
 
-        let income = data.totalIncome()
-        let expenses = data.totalExpenses()
+        let totalIncome = expenses.totalIncome()
+        let totalExpenses = expenses.totalExpenses()
 
-        let summary = Summary(income: income, expenses: expenses, balance: (income - expenses))
+        let summary = Summary(income: totalIncome, expenses: totalExpenses)
 
-        return ExpensesListData(expenses: data.expenses, summary: summary)
+        return ExpensesListData(expenses: expenses.toExpensesByDayArray(), summary: summary)
+    }
+}
+
+// TODO: Remove
+extension String {
+
+    func toDate() -> Date {
+        let dateFormater = DateFormatter()
+        dateFormater.dateFormat = "dd,MM,yyyy"
+        return dateFormater.date(from: self)!
     }
 }
