29f590a11282c7db11f66ad37164645798ffc738
Julian Centurion <julian.centurion@mercadolibre.com>
2022-06-28 14:03:49 -0300

    Add loadNib

--- ExpensesReportTests/Extensions/UIView+Tests.swift
+++ ExpensesReportTests/Extensions/UIView+Tests.swift
@@ -5,4 +5,13 @@
 //  Created by Julian Centurion on 28/06/2022.
 //
 
-import Foundation
+import UIKit
+
+extension UIView {
+
+    static func loadNib() -> Self {
+        // swiftlint:disable force_cast
+        Bundle.main.loadNibNamed("\(Self.self)", owner: nil, options: nil)?.first as! Self
+        // swiftlint:enable force_cast
+    }
+}
