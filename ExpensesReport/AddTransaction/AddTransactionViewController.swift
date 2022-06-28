571736720d79f7161f47c6e1935e83ace4b110c6
Julian Centurion <julian.centurion@mercadolibre.com>
2022-06-28 17:00:07 -0300

    Fix UI and close gesture

--- ExpensesReport/AddTransaction/AddTransactionViewController.swift
+++ ExpensesReport/AddTransaction/AddTransactionViewController.swift
@@ -9,20 +9,15 @@ import UIKit
 
 class AddTransactionViewController: UIViewController {
 
+    @IBOutlet weak var containerView: UIView!
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        containerView.rondedView()
+        let backgroundTap = UITapGestureRecognizer(target: self, action: #selector(dismissController))
+        view.addGestureRecognizer(backgroundTap)
     }
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @objc func dismissController() {
+        dismiss(animated: true, completion: nil)
     }
-    */
-
 }
