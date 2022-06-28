92a55bcead7e3f9e697e27c4eb5d6b85bcd53cf0
Julian Centurion <julian.centurion@mercadolibre.com>
2022-06-28 18:13:20 -0300

    Create TransactionInputValidation

---
+++ ExpensesReport/AddTransaction/Validation/TransactionInputValidation.swift
@@ -0,0 +1,32 @@
+//
+//  TransactionInputValidation.swift
+//  ExpensesReport
+//
+//  Created by Julian Centurion on 28/06/2022.
+//
+
+import Foundation
+
+class TransactionInputValidation {
+
+    static func validateInput(amount: String?,
+                              description: String?,
+                              transactionType: TransactionType?) -> (Bool, Transaction?) {
+        guard let amount = amount,
+              let amountFloat = Float(amount),
+              amountFloat > 0,
+              let description = description,
+              description.count > 0,
+              let transactionType = transactionType
+        else {
+            return (false, nil)
+        }
+
+        let transaction = Transaction(description: description,
+                                      date: Date.now,
+                                      amount: amountFloat,
+                                      transaction: transactionType)
+
+        return(true, transaction)
+    }
+}
