95dec3a8497609d360d1c1b40f8f4274a9b3a615
Julian Centurion <julian.centurion@mercadolibre.com>
2022-06-28 11:26:47 -0300

    Add date tessts

--- ExpensesReportTests/Extensions/DateTests.swift
+++ ExpensesReportTests/Extensions/DateTests.swift
@@ -6,30 +6,34 @@
 //
 
 import XCTest
+@testable import ExpensesReport
 
 class DateTests: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
+    func testParseDateFormat() throws {
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
+        var date = "12/01/1990".toDate()
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
+        XCTAssertEqual("12th January, 1990", date.toString())
 
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
+        date = "1/01/1990".toDate()
 
+        XCTAssertEqual("1st January, 1990", date.toString())
+
+        date = "21/01/1990".toDate()
+
+        XCTAssertEqual("21st January, 1990", date.toString())
+
+        date = "23/01/1990".toDate()
+
+        XCTAssertEqual("23rd January, 1990", date.toString())
+
+        date = "2/01/1990".toDate()
+
+        XCTAssertEqual("2nd January, 1990", date.toString())
+
+        date = "3/01/1990".toDate()
+
+        XCTAssertEqual("3rd January, 1990", date.toString())
+    }
 }
