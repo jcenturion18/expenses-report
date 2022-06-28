5a7390e1c77c642da5dc0f1d41bd77df8e0835df
Julian Centurion <julian.centurion@mercadolibre.com>
2022-06-28 14:04:05 -0300

    Create tests set

--- ExpensesReportTests/Cells/ExpenseTableViewCellTests.swift
+++ ExpensesReportTests/Cells/ExpenseTableViewCellTests.swift
@@ -6,30 +6,47 @@
 //
 
 import XCTest
+@testable import ExpensesReport
 
 class ExpenseTableViewCellTests: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    func testSetUpViewWithExpenseIncome() throws {
+
+        let cell = ExpenseTableViewCell.loadNib()
+
+        let expense = Expense(description: "Description",
+                              date: "12/01/1990".toDate(),
+                              amount: 10,
+                              transaction: .income)
+
+        cell.setUp(withExpense: expense)
+
+        XCTAssertEqual("Description", cell.titleLabel.text)
+        XCTAssertEqual("$10.0", cell.accesoryLabel.text)
     }
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    func testSetUpViewWithExpenseNegativeExpense() throws {
+        let cell = ExpenseTableViewCell.loadNib()
+
+        let expense = Expense(description: "Description",
+                              date: "12/01/1990".toDate(),
+                              amount: 10,
+                              transaction: .expense)
+
+        cell.setUp(withExpense: expense)
+
+        XCTAssertEqual("Description", cell.titleLabel.text)
+        XCTAssertEqual("- $10.0", cell.accesoryLabel.text)
     }
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
+    func testSetUpViewWithExpensesByDay() throws {
+        let cell = ExpenseTableViewCell.loadNib()
 
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
+        let expensesByDay = ExpensesByDay(title: "Day", expenses: [])
 
+        cell.setUp(withExpensesByDay: expensesByDay)
+
+        XCTAssertEqual("Day", cell.titleLabel.text)
+        XCTAssertEqual("", cell.accesoryLabel.text)
+    }
 }
