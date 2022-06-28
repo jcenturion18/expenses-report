8e592d459c1d9cf99fe600ea60ba41e872d8b684
Julian Centurion <julian.centurion@mercadolibre.com>
2022-06-28 10:21:48 -0300

    Add date format

--- ExpensesReport/Extensions/Date.swift
+++ ExpensesReport/Extensions/Date.swift
@@ -11,7 +11,28 @@ extension Date {
 
     func toString() -> String {
         let dateFormater = DateFormatter()
-        dateFormater.dateFormat = "dd,MM,yyyy"
+        dateFormater.locale = Locale(identifier: "en-US")
+        dateFormater.dateFormat = formatedString()
         return dateFormater.string(from: self)
+    }
+
+    private func formatedString() -> String {
+        return "dd'\(self.daySuffix())' MMMM, yyyy"
+    }
+
+    private func daySuffix() -> String {
+        let calendar = Calendar.current
+        let components = (calendar as NSCalendar).components(.day, from: self)
+        let dayOfMonth = components.day
+        switch dayOfMonth {
+        case 1, 21, 31:
+            return "st"
+        case 2, 22:
+            return "nd"
+        case 3, 23:
+            return "rd"
+        default:
+            return "th"
+        }
     }
 }
