d7af36822973c94b67dbfd0193818a69bf9dbb18
Julian Centurion <julian.centurion@mercadolibre.com>
2022-06-30 14:35:47 -0300

    Lint

--- ExpensesReportTests/Validation/TransactionInputValidationTests.swift
+++ ExpensesReportTests/Validation/TransactionInputValidationTests.swift
@@ -10,42 +10,46 @@ import XCTest
 
 class TransactionInputValidationTests: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
     func testInvalidCases() throws {
 
-        var result = TransactionInputValidation.validateInput(amount: nil, description: nil, transactionType: nil)
+        var result = TransactionInputValidation.validateInput(amount: nil,
+                                                              description: nil,
+                                                              transactionType: nil)
 
         XCTAssertFalse(result.0)
         XCTAssertNil(result.1)
 
-        result = TransactionInputValidation.validateInput(amount: "nil", description: nil, transactionType: nil)
+        result = TransactionInputValidation.validateInput(amount: "nil",
+                                                          description: nil,
+                                                          transactionType: nil)
 
         XCTAssertFalse(result.0)
         XCTAssertNil(result.1)
 
-        result = TransactionInputValidation.validateInput(amount: "123", description: nil, transactionType: nil)
+        result = TransactionInputValidation.validateInput(amount: "123",
+                                                          description: nil,
+                                                          transactionType: nil)
 
         XCTAssertFalse(result.0)
         XCTAssertNil(result.1)
 
-        result = TransactionInputValidation.validateInput(amount: "0", description: nil, transactionType: nil)
+        result = TransactionInputValidation.validateInput(amount: "0",
+                                                          description: nil,
+                                                          transactionType: nil)
 
         XCTAssertFalse(result.0)
         XCTAssertNil(result.1)
 
-        result = TransactionInputValidation.validateInput(amount: "123", description: "", transactionType: nil)
+        result = TransactionInputValidation.validateInput(amount: "123",
+                                                          description: "",
+                                                          transactionType: nil)
 
         XCTAssertFalse(result.0)
         XCTAssertNil(result.1)
 
-        result = TransactionInputValidation.validateInput(amount: "123", description: "Valid text", transactionType: nil)
+        result = TransactionInputValidation.validateInput(amount: "123",
+                                                          description: "Valid text",
+                                                          transactionType: nil)
 
         XCTAssertFalse(result.0)
         XCTAssertNil(result.1)
@@ -53,7 +57,9 @@ class TransactionInputValidationTests: XCTestCase {
 
     func testValidCases() throws {
 
-        let result = TransactionInputValidation.validateInput(amount: "123", description: "Valid", transactionType: .income)
+        let result = TransactionInputValidation.validateInput(amount: "123",
+                                                              description: "Valid",
+                                                              transactionType: .income)
 
         XCTAssertTrue(result.0)
         XCTAssertNotNil(result.1)
