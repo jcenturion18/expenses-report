7703d87503ca4258866ff114719c2b074112293b
Julian Centurion <julian.centurion@mercadolibre.com>
2022-06-30 14:35:17 -0300

    Start using DataBase

--- ExpensesReport/TransactionList/TransactionListViewModel.swift
+++ ExpensesReport/TransactionList/TransactionListViewModel.swift
@@ -9,6 +9,7 @@ import Foundation
 
 protocol TransactionListViewModel: AnyObject {
     var bindViewModelToController : ((_ transactionListData: TransactionListData) -> Void) { get set }
+    init (withDB database: DataBase)
     func loadTransactionsData()
     func transactionCreated(_ transaction: Transaction)
     func deleteTransaction(at indexPath: IndexPath)
@@ -16,13 +17,12 @@ protocol TransactionListViewModel: AnyObject {
 
 class TransactionListViewModelImp: TransactionListViewModel {
 
-    private var transactions = [
-        Transaction(description: "Salary", date: "30/05/2022".toDate(), amount: 10, transaction: .income),
-        Transaction(description: "Coffee", date: "26/05/2022".toDate(), amount: 7, transaction: .expense),
-        Transaction(description: "Salary", date: "26/05/2022".toDate(), amount: 10, transaction: .income)
-    ] {
-        didSet {
-            loadTransactionsData()
+    let database: DataBase
+
+    required init(withDB database: DataBase = DataBaseRealmImp()) {
+        self.database = database
+        self.database.observer = { transactions in
+            self.transactionListData = self.proccess(transactions: transactions)
         }
     }
 
@@ -35,7 +35,7 @@ class TransactionListViewModelImp: TransactionListViewModel {
     var bindViewModelToController : ((_ transactionListData: TransactionListData) -> Void) = {_ in }
 
     func loadTransactionsData() {
-        transactionListData = proccess(transactions: transactions)
+        transactionListData = proccess(transactions: database.retrieveTransactions())
     }
 
     func proccess(transactions: [Transaction]) -> TransactionListData {
@@ -49,23 +49,12 @@ class TransactionListViewModelImp: TransactionListViewModel {
     }
 
     func deleteTransaction(at indexPath: IndexPath) {
-        transactionListData.transactions[indexPath.section].transactions.remove(at: indexPath.row - 1)
-        if transactionListData.transactions[indexPath.section].transactions.count == 0 {
-            transactionListData.transactions.remove(at: indexPath.section)
-        }
+        database.delete(transaction:
+                            transactionListData.transactions[indexPath.section].transactions[indexPath.row - 1]
+        )
     }
 
     func transactionCreated(_ transaction: Transaction) {
-        transactions.append(transaction)
-    }
-}
-
-// TODO: Remove
-extension String {
-
-    func toDate() -> Date {
-        let dateFormater = DateFormatter()
-        dateFormater.dateFormat = "dd,MM,yyyy"
-        return dateFormater.date(from: self)!
+        database.store(transaction: transaction)
     }
 }
