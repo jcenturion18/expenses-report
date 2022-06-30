2f28d548778fbce810699440934c0b36ef89a3d5
Julian Centurion <julian.centurion@mercadolibre.com>
2022-06-30 14:32:42 -0300

    Create TransactionDAO

--- ExpensesReport/Database/TransactionDAO.swift
+++ ExpensesReport/Database/TransactionDAO.swift
@@ -6,3 +6,32 @@
 //
 
 import Foundation
+import RealmSwift
+
+class TransactionDAO: Object {
+    @Persisted var transactionDescription: String
+    @Persisted var date: Date
+    @Persisted var amount: Float
+    @Persisted var transactionType: String
+    @Persisted var id: String
+
+    static func from(_ transaction: Transaction) -> TransactionDAO {
+        let transactionDAO = TransactionDAO()
+        transactionDAO.transactionDescription = transaction.description
+        transactionDAO.date = transaction.date
+        transactionDAO.amount = transaction.amount
+        transactionDAO.transactionType = transaction.type.rawValue
+        transactionDAO.id = transaction.id
+        return transactionDAO
+    }
+
+    func toTransaction() -> Transaction {
+        return Transaction(
+            description: transactionDescription,
+            date: date,
+            amount: amount,
+            type: TransactionType.init(rawValue: transactionType)!,
+            id: id
+        )
+    }
+}
