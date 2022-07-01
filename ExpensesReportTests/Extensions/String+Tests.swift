38dfc8fb6474001cf95d0ee80b44f828b889903d
Julian Centurion <julian.centurion@mercadolibre.com>
2022-06-30 21:33:55 -0300

    Add toDate

--- ExpensesReportTests/Extensions/String+Tests.swift
+++ ExpensesReportTests/Extensions/String+Tests.swift
@@ -6,3 +6,12 @@
 //
 
 import Foundation
+
+extension String {
+
+    func toDate() -> Date {
+        let dateFormater = DateFormatter()
+        dateFormater.dateFormat = "dd/MM/yyyy"
+        return dateFormater.date(from: self)!
+    }
+}
