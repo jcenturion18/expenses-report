2c3f7e0e2df1677811c5e1241255195e99ea8ff2
Julian Centurion <julian.centurion@mercadolibre.com>
2022-06-30 21:57:18 -0300

    Create db mock

--- ExpensesReportTests/Mocks/DataBaseMockImp.swift
+++ ExpensesReportTests/Mocks/DataBaseMockImp.swift
@@ -6,3 +6,23 @@
 //
 
 import Foundation
+@testable import ExpensesReport
+
+class DataBaseMockImp: DataBase {
+
+    var transactions: [Transaction] = []
+
+    func retrieveTransactions() -> [Transaction] {
+        transactions
+    }
+
+    func store(transaction: Transaction) {
+        transactions.append(transaction)
+    }
+
+    func delete(transaction: Transaction) {
+        transactions.removeAll { $0.id == transaction.id }
+    }
+
+    var observer: (([Transaction]) -> Void)?
+}
